912c11f8094bb4cd2fc59652fa28af1b2602a3fe
Derek Harris <testuser@testaddress.com>
2016-07-25 13:33:59 +1000

    Change GPIO Pin for buzzer & add comments
    Change the BZRPin from GPIO Pin 11 to Pin 12 to match the diagram in Lesson 3
     Adeept_RFID_LearningKit_for_RPi.pdf (not dated, unknown version)

--- 03_passiveBuzzer.py
+++ 03_passiveBuzzer.py
@@ -2,23 +2,23 @@
 import RPi.GPIO as GPIO
 import time
 
-BZRPin = 11
+BZRPin = 12     # Set Pin Phyiscal Pin 12
 
 GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
 GPIO.setup(BZRPin, GPIO.OUT)   # Set pin mode as output
-GPIO.output(BZRPin, GPIO.LOW)
+GPIO.output(BZRPin, GPIO.LOW)   # Set Buzzer to Off
 
 p = GPIO.PWM(BZRPin, 50) # init frequency: 50HZ
 p.start(50)  # Duty cycle: 50%
 
 try:
 	while True:
-		for f in range(100, 2000, 100):
+		for f in range(100, 2000, 100):     # Implements a loop increasing frequency
 			p.ChangeFrequency(f)
 			time.sleep(0.2)
-		for f in range(2000, 100, -100):
+		for f in range(2000, 100, -100):    # Implememnts a loop decreasing frequency
 			p.ChangeFrequency(f)
 			time.sleep(0.2)
-except KeyboardInterrupt:
+except KeyboardInterrupt:       # When a keyboard interrupt is detected, stop the while loop and cleanup
 	p.stop()
-	GPIO.cleanup()
+	GPIO.cleanup()  # Release GPIO resources
