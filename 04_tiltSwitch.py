8db77e7dd5ef8391039d81f5936169689aa6154a
adeept <adeept@163.com>
2017-06-05 07:09:51 +0000

    OK...

--- 04_tiltSwitch.py
+++ 04_tiltSwitch.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 
-TiltPin = 11
-LedPin  = 12
-
-Led_status = 1
+LedPin  = 11
+TiltPin = 12
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
@@ -12,19 +10,14 @@ def setup():
 	GPIO.setup(TiltPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.output(LedPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the led
 
-def swLed(ev=None):
-	global Led_status
-	Led_status = not Led_status
-	GPIO.output(LedPin, Led_status)  # switch led status(on-->off; off-->on)
-	if Led_status == 1:
-		print 'led off...'
-	else:
-		print '...led on'
-
 def loop():
-	GPIO.add_event_detect(TiltPin, GPIO.FALLING, callback=swLed) # wait for falling
 	while True:
-		pass   # Don't do anything
+		if GPIO.input(TiltPin) == GPIO.LOW:
+			GPIO.output(LedPin, GPIO.LOW)
+			print 'LED ON...'
+		else:
+			GPIO.output(LedPin, GPIO.HIGH)
+			print '...LED OFF'
 
 def destroy():
 	GPIO.output(LedPin, GPIO.HIGH)     # led off
