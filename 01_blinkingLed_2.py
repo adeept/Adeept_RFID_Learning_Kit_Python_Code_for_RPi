2b117e48e8106174559007303ca17d3fa0246c3c
CHRIS <hijnxbusiness@gmail.com>
2017-09-06 14:15:16 +0800

    changed timing and comments

--- 01_blinkingLed_2.py
+++ 01_blinkingLed_2.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 
-LedPin = 11    # pin11 -- testing branches here too and now using ADD
+LedPin = 12    # pin12 -- new pin declaration
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
@@ -13,10 +13,10 @@ def loop():
 	while True:
 		print '...led on'
 		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
+		time.sleep(1)				   # increase time to slow down blink
 		print 'led off...'
 		GPIO.output(LedPin, GPIO.HIGH) # led off
-		time.sleep(0.5)
+		time.sleep(1)                  # increase timing to slow down blink
 
 def destroy():
 	GPIO.output(LedPin, GPIO.HIGH)     # led off
