b75b880ddc7ea13fe45cb77442e1600b549760ea
mitchel <mitchel_king@icloud.com>
2018-03-22 18:32:02 +0800

    Change pins to suit my wiring, add time.sleep to free up the CPU, so it does not get hung

--- 05_btnAndLed_1.py
+++ 05_btnAndLed_1.py
@@ -10,25 +10,27 @@
 #-----------------------------------------------------------
 
 import RPi.GPIO as GPIO
+import time
 
-LedPin = 11    # pin11 --- led
-BtnPin = 12    # pin12 --- button
+LedPin = 37    # pin37 --- led
+BtnPin = 40    # pin40 --- button
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
 	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
-	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to make led off
+	GPIO.output(LedPin, GPIO.LOW) # Set LedPin LOW(+3.3V) to make led off
 
 def loop():
 	while True:
-		if GPIO.input(BtnPin) == GPIO.LOW: # Check whether the button is pressed or not.
+		if GPIO.input(BtnPin) == GPIO.HIGH: # Check whether the button is pressed or not.
 			print '...led on'
-			GPIO.output(LedPin, GPIO.LOW)  # led on
+			GPIO.output(LedPin, GPIO.HIGH)  # led on
+			time.sleep(0.3)
 		else:
 			print 'led off...'
-			GPIO.output(LedPin, GPIO.HIGH) # led off
-
+			GPIO.output(LedPin, GPIO.LOW) # led off
+			time.sleep(0.3)
 def destroy():
 	GPIO.output(LedPin, GPIO.HIGH)     # led off
 	GPIO.cleanup()                     # Release resource
